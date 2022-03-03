7eadf519a12269e360e6b65f4c042b950d93079e
valeriamrk <87308393+valeriamrk@users.noreply.github.com>
2022-03-04 01:45:55 +0300

    stats slice
    stats slice added

--- src/components/presentational/molecules/logoGroup/LogoGroup.jsx
+++ src/components/presentational/molecules/logoGroup/LogoGroup.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { Logo } from "../../../presentational";
 import * as S from "./styles";
+import { useNavigate } from "react-router-dom";
 
 const LogoGroup = () => {
+  const navigate = useNavigate();
+  const path = "";
+  const handleItemClick = (event, path) => {
+    navigate(path);
+  };
+
   return (
-    <S.LogoGroup>
+    <S.LogoGroup onClick={(event) => handleItemClick(event, path)}>
       <Logo />
       <S.LogoText>OpenOcean</S.LogoText>
     </S.LogoGroup>
